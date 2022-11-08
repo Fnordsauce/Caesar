f50ec9bc1cac98fe13d07b0b38409ff161e408e9
Fnordsauce <ayden.ostoich@gmail.com>
2022-11-07 22:37:34 -0500

    tabual

--- WS_Tracker/currentPeriod_Activity.py
+++ WS_Tracker/currentPeriod_Activity.py
@@ -1,4 +1,15 @@
 import pandas as pd
+import tabula
 from tabula import read_pdf
-dfs = tabula.read_pdf("Personal_Project\WS_Tracker\Performance_Statements\OCT22.pdf", multiple_tables=True, pages="all", encoding="utf-8")
+dfs = tabula.read_pdf("WS_Tracker\Performance_Statements\OCT22.PDF",stream=True, multiple_tables=True, pages="3", encoding="utf-8")
 
+
+count = 0
+for df in dfs:
+    if count == 0:
+        data = df
+    count+=1
+data.columns = data.iloc[0]
+data = data.reindex(data.index.drop(0)).reset_index(drop=True)
+data.columns.name = None
+print(data)
